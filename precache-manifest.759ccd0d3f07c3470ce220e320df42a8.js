daeed67df363f1839449a7e56c9077a1dfa41c70
naveen <naveen.hn.kumar@gmail.com>
2018-12-20 01:27:10 -0700

    Updates

---
+++ precache-manifest.759ccd0d3f07c3470ce220e320df42a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "697ba5e77a00dad0d255",
+    "url": "/brewerysearchuidemo/static/css/main.e45c57c3.chunk.css"
+  },
+  {
+    "revision": "697ba5e77a00dad0d255",
+    "url": "/brewerysearchuidemo/static/js/main.697ba5e7.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/brewerysearchuidemo/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "a6610c50847b8b4c1b1d",
+    "url": "/brewerysearchuidemo/static/js/runtime~main.a6610c50.js"
+  },
+  {
+    "revision": "73dcf2ea883d695cb091ea3c6c7838c5",
+    "url": "/brewerysearchuidemo/index.html"
+  }
+];
