19306eaf06ab20b27bab26a0ea89d6efad7e98b0
naveen <naveen.hn.kumar@gmail.com>
2018-12-20 05:01:37 -0700

    Updated to handle search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './retrobeer.png';
 import './App.css';
 
+
+// const API_URL="http://localhost:8080/breweries",
+const API_URL = "https://murmuring-basin-65291.herokuapp.com/breweries?page=1&count=18";
+const API_SEARCH_URL = "https://murmuring-basin-65291.herokuapp.com/breweries/search?page=1&count=18&query=";
+const DEFAULT_QUERY = "Phoenix";
+
 class App extends Component {
 
 
@@ -46,9 +52,13 @@ class Content extends Component {
       isLoading: true,
       items: [],
       error: null,
-      // apiUrl:"http://localhost:8080/breweries",
-      apiUrl: "https://murmuring-basin-65291.herokuapp.com/breweries?page=1&count=25",
+      query: DEFAULT_QUERY
+
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   componentDidMount() {
@@ -56,28 +66,9 @@ class Content extends Component {
 
   }
 
-  /*
-  fetchBreweries2(){
-    axios.get(this.state.apiUrl, {crossDomain:true})
-      
-      .then(response => 
-        this.setState({ 
-          items: response.data, 
-          isLoading: false 
-        })
-        )
-      .catch(error => 
-        this.setState({
-          error, 
-          isLoading:false
-        })
-      );
-  }
-  */
-
 
   fetchBreweries() {
-    fetch(this.state.apiUrl)
+    fetch(API_URL)
       .then(response => response.json())
       .then(data =>
         this.setState({
@@ -93,34 +84,68 @@ class Content extends Component {
       );
   }
 
+  handleInputChange(e) {
+    this.setState({
+      query: e.target.value
+    });
+
+  }
+
+  
+
+  handleSearch() {
+
+    fetch(API_SEARCH_URL + this.state.query)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          items: data,
+          isLoading: false
+        })
+      )
+      .catch(error =>
+        this.setState({
+          error,
+          isLoading: false
+        })
+      );
+
+  }
+
+
+  handleSubmit(e) {
+     e.preventDefault();
+     this.handleSearch();
+  }
+
 
   render() {
 
     const { isLoading, items, error } = this.state;
     return (
       <div className="content pure-u-1 pure-u-md-3-4">
-        <div>
         <div class="content.subheader">
-            <h1>Next Beer!</h1>
-          </div>
+          <h1>Next Beer!</h1>
+        </div>
+        <div className="searchform">
+          <form className="pure-form" onSubmit={this.handleSubmit}>
+            <fieldset>
+              <input type="text" className="pure-input-rounded" placeholder="search.." onChange={this.handleInputChange} /> &nbsp;
+                <button type="button" className="pure-button pure-button-primary" onClick={this.handleSearch}>Search</button>
+            </fieldset>
+          </form>
+        </div>
 
-          <div className="searchform">
-            <form className="pure-form">
-              <fieldset>
-                <input type="text" className="pure-input-rounded" placeholder="search.." /> &nbsp;
-                <button type="submit" className="pure-button pure-button-primary">Search</button>
-              </fieldset>
-            </form>
-          </div>
 
-         
-          {error ? <p>{error.message}</p> : null}
+        {error ? <p>{error.message}</p> : null}
 
-          {/**********Table to Display ************/}
-          <table class="pure-table pure-table-horizontal">
+        {/**********Table to Display ************/}
+        <div className="results">
+          <table className="pure-table pure-table-horizontal">
             <thead>
               <tr>
                 <th>Name</th>
+                <th>Type</th>
                 <th>City</th>
                 <th>State</th>
                 <th>Phone</th>
@@ -129,11 +154,12 @@ class Content extends Component {
             </thead>
             <tbody>
               {items.map(brewery => {
-                const { id, name, city, state, phone, websiteUrl } = brewery;
+                let { name, breweryType, city, state, phone, websiteUrl } = brewery;
                 return (
 
                   <tr>
                     <td>{name}</td>
+                    <td>{breweryType}</td>
                     <td>{city}</td>
                     <td>{state}</td>
                     <td>{phone}</td>
@@ -146,8 +172,6 @@ class Content extends Component {
               })}
             </tbody>
           </table>
-
-
 
         </div>
 
